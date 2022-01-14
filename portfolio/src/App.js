ca819ceb14cbc4a31ac7a55585fb8d6cb089df22
Lesly <leslyr562@gmail.com>
2022-01-14 16:22:39 -0600

    added picture

---
+++ portfolio/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Portfolio from './components/Project';
+import coverImage from "../../assets/cover/cover-image.jpg";
+
+
+function App() {
+
+  return (
+    <div>
+      <main>
+        <Portfolio></Portfolio>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/Project/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import coverImage from "../../assets/cover/cover-image.jpg"
+
+function Portfolio() {
+  return (
+    <section>
+      <h1 id="portfolio">Who am I?</h1>
+      <img src={coverImage}  style={{ width: "100%" }} alt="cover" />
+    </section>
+  );
+}
+
+export default Portfolio;
