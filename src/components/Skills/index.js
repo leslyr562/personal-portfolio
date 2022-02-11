25a705cec35eb7e5cdce71f8c83106dd5a68b075
Lesly <leslyr562@gmail.com>
2022-02-11 00:34:38 -0600

    added skill bar

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,37 +1,65 @@
 import React from "react";
+import {
+    FaHtml5,
+    FaCss3Alt,
+    FaJs,
+    FaBootstrap,
+    FaReact,
+    FaNodeJs,
+
+} from "react-icons/fa";
+import { SiHandlebarsdotjs, SiMongodb, SiMysql, SiExpress, SiApollographql } from "react-icons/si";
+
 
 function skills() {
 
-    return(
+    return (
         <div id="skills">
             <h1 className='sectionName'>Skills</h1>
+
             <div>
-                <div>
-                    <div><a href={require(`../../assets/documents/Resume.pdf`)} className="resume-link">Resume Link</a></div>
-                    <div className="language">
-                        <h3>Front-End Proficiencies</h3>
-                        <ul className="front-end">
-                            <li>HTML</li>
-                            <li>CSS</li>
-                            <li>Bootstrap</li>
-                            <li>JavaScript</li>
-                            <li>Handlebars</li>
-                            <li>React</li>
-                        </ul>
-                    </div>
-                    <div className="language">
-                        <h3>Back-End Proficiencies</h3>
-                        <ul className="back-end">
-                            <li>APIs</li>
-                            <li>Node</li>
-                            <li>Express</li>
-                            <li>MySQL, Sequelize</li>
-                            <li>MongoDB, Mongoose</li>
-                            <li>REST</li>
-                            <li>GraphQL</li>
-                        </ul>
-                    </div>
+                <div><a href={require(`../../assets/documents/Resume.pdf`)} className="resume-link">Resume Link</a></div>
+
+                <div class="w3-container w3-pink strip">
+                    <ul className='strip'> 
+                        <li className='flist svg_icons'>
+                            <FaHtml5 />
+                        </li>
+                        <li className='flist svg_icons'>
+                            <FaCss3Alt />
+                        </li>
+                        <li className='flist svg_icons'>
+                            <FaJs />
+                        </li>
+                        <li className='flist svg_icons'>
+                            <FaBootstrap />
+                        </li>
+                        <li className='flist svg_icons'>
+                            <SiHandlebarsdotjs />
+                        </li>
+                        <li className='flist svg_icons'>
+                            <FaReact />
+                        </li>
+                        <li className='flist svg_icons'>
+                            <FaNodeJs />
+                        </li>
+                        <li className='flist svg_icons'>
+                            <SiMongodb />
+                        </li>
+                        <li className='flist svg_icons'>
+                            <SiMysql />
+                        </li>
+                        <li className='flist svg_icons'>
+                            <SiExpress />
+                        </li>
+                        <li className='flist svg_icons'>
+                            <SiApollographql />
+                        </li>
+
+                    </ul>
                 </div>
+
+
             </div>
         </div>
     );
