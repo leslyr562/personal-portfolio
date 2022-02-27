96288ed3793d3c0965785d403d68a126ba8b3130
Lesly <leslyr562@gmail.com>
2022-02-27 17:47:42 -0600

    added to my projects

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -9,29 +9,24 @@ function Portfolio() {
       name: 'Budget Tracker',
       class: 'budget',
       category: 'projects',
+      technologies: ['HTML', 'CSS', 'JavaScript', 'Node.js', 'express', 'mongoDB', 'mongoose', 'indexDB', 'service worker'],
       description: 'PWA Offline small application that tracks financial transactions with a graph and list even with no internet connection',
       github: 'https://github.com/leslyr562/Budget-Tracker-Offline.git',
       url:'https://guarded-waters-52170.herokuapp.com/'
     },
     {
-      name: 'Take Notes',
-      class: 'notes',
+      name: 'Shop Shop',
+      class: 'shop',
       category: 'projects',
-      description: 'An application where crating and updating notes as a reminder',
-      github: 'https://github.com/leslyr562/Take-notes.git',
-      url:'https://lit-crag-71791.herokuapp.com/'
-    },
-    {
-      name: 'Daily Planner',
-      class: 'planner',
-      category: 'projects',
-      description: 'Plan the day hourly with current date and time',
-      github:'https://github.com/leslyr562/daily-planner.git',
-      url: 'https://leslyr562.github.io/daily-planner/'
+      technologies: ['React', 'Redux', 'CSS3', 'JavaScript','Node.js', 'express', 'graphql', 'apollo-server', 'mongoDB', 'nodemon', 'bcrypt', 'jsonwebtoken', 'Stripe'],
+      description: 'E-commerce shopping application ',
+      github: 'https://github.com/leslyr562/shop-shop.git',
+      url:'https://shoping-shop.herokuapp.com/'
     },
     {
       name: 'Worldwide Weather Forecast',
       class: 'forecast',
+      technologies: ['HTML5', 'CSS3', 'Bootstrap', 'JavaScript'],
       category: 'projects',
       description: 'Check the weather from any city',
       github: 'https://leslyr562.github.io/Worldwide-Weather-Forecast/',
@@ -41,6 +36,7 @@ function Portfolio() {
       name: 'Running On Empty',
       class: 'running',
       category: 'projects',
+      technologies: ['React.js', 'Express', 'jsonwebtoken', 'bcrypt', 'Apollo Server', 'Graphql', 'MongoDB', 'Node.JS', 'Semantic UI'],
       description: 'Social Running/ Drinking Event Application',
       github: 'https://github.com/rprice000/running-on-empty.git',
       url: 'https://gentle-depths-07774.herokuapp.com/'
@@ -48,6 +44,7 @@ function Portfolio() {
     {
       name: 'Tech Ticket System',
       class: 'tech',
+      technologies:['HTML5', 'CSS3', 'Javascript', 'Node.js', 'Express', 'Express-Session', 'Express-Handlebars', 'Connect-Session-Sequelize', 'Bcrypt', 'Sequelize', 'dotenv', 'NodeMailer', 'MySQL'],
       category: 'projects',
       description: 'Technology ticket system would enable technology teams to capture technical issues, user requests, and other tasks in a document called a ticket. ',
       github: 'https://github.com/rprice000/tech_ticket_system.git',
